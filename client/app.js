ea0f422bc88c76f3227e59cb0f31affa82b3ab4d
Hyun647 <chodohyun0407@icloud.com>
2024-09-16 14:36:22 +0900

    m

--- client/app.js
+++ client/app.js
@@ -4,19 +4,14 @@ const socket = new WebSocket('ws://110.15.29.199:3001');
 
 const memo = document.getElementById('memo');
 
+// 서버에서 받은 메모를 표시
+socket.onmessage = (event) => {
+    // 서버에서 받은 데이터를 텍스트로 처리
+    memo.value = event.data;
+};
+
 // 메모가 변경될 때 서버로 전송
 memo.addEventListener('input', () => {
     const message = memo.value;
     socket.send(message);
 });
-
-// 서버에서 받은 메모를 표시
-socket.onmessage = (event) => {
-    const reader = new FileReader();
-    
-    reader.onload = () => {
-        memo.value = reader.result;  // 텍스트로 표시
-    };
-    
-    reader.readAsText(event.data);  // Blob 데이터를 텍스트로 변환
-};
