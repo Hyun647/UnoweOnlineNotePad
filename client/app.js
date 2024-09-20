7971b71ab94beb0eddb8ad8a7ec3e2bf0f497331
Hyun647 <chodohyun0407@icloud.com>
2024-09-20 16:39:03 +0900

    Update app.js

--- client/app.js
+++ client/app.js
@@ -52,7 +52,7 @@ const attemptReconnect = () => {
         reconnectInterval = setInterval(() => {
             console.log('서버 재연결 시도 중...');
             socketConnect();
-        }, 5000); // 5초마다 재연결 시도
+        }, 3000); // 3초마다 재연결 시도
     }
 };
 
